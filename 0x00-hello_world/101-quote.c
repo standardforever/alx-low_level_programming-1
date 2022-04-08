98a258786e9ccdb5e4f96f83754e6ee25559e25b
standardforever <standard.forever123@gmail.com>
2022-04-07 20:46:58 -0700

    up_to_date

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - print in the std:error
- *Return: return 1
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+       write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+       return (1);
 }
+
+
