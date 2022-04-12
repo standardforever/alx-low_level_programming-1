10b7b8db180423dfe1a174951ba82ac9c224203f
standardforever <standard.forever123@gmail.com>
2022-04-12 18:27:50 +0100

    updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,55 +1,18 @@
-#include <unistd.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and err no is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
 
-/**
- * _islower_lower - write the lower case latter to the stdout
- * Return: return 0
- */
-
-int _islower_case(int c)
-{
-	return (islower(c));
-}
-
-/**
- * _isalpha_case - write the alphabetic character
- * Return: it returns 0
- */
-int _isalpha_case(int a)
-{
-	return (isalpha(a));
-}
-
-/**
- * print - write to stdout
- * Return: 0
- */
-
-char print(char b)
-{
-	return (printf("%c", b));
-}
-
-int abso(int d)
-{
-	return (abs(d));
-}
-
-int pri(int f)
-{
-	return (printf("%d", f));
-}
+#endif
