d1d30a89faa3aaea56539552dd581614286753ad
standardforever <standard.forever123@gmail.com>
2022-04-12 14:25:17 -0700

    Hot fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,30 @@
 #include "main.h"
 
 /**
- * print_sign - print the sign of the number
- * @n: the argument
- *
- * Return: return 0
+ * print_sign - check numers
+ * @n : number to check
+ * Return:0 or 1
  */
+
 int print_sign(int n)
 {
-	if (n > 1)
+	int test;
+
+	if (n > 0)
 	{
+		test = 1;
 		_putchar('+');
-		return (1);
 	}
 	else if (n == 0)
 	{
+		test = 0;
 		_putchar('0');
-		return (0);
 	}
-	else if  (n < 0)
+	else
 	{
+		test = -1;
 		_putchar('-');
-		return (-1);
 	}
+
+	return (test);
 }
