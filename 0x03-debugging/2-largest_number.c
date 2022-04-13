223d627411d1fbee969a89f007f478449d533c7c
standardforever <standard.forever123@gmail.com>
2022-04-13 12:14:46 -0700

    up_to_date

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -19,9 +19,15 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if (c > a && a > b)
+	else if (c > a  && a > b)
 	{
 		largest = c;
 	}
+	else if (b > c && c > a)
+		largest = b;
+	else if (a > c && c > b)
+		largest = c;
+	else if (c > b && b > a)
+		largest = c;
 	return (largest);
 }
