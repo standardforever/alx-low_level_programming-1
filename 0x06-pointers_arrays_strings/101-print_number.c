3c9c27e08bc89215b04ff10e4d074e9b45cbc8ba
standardforever <standard.forever123@gmail.com>
2022-04-24 03:20:29 -0700

    updated

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,15 +7,16 @@
  */
 void print_number(int n)
 {
+	unsigned int i = n;
 	/*print '-' for negative number */
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		i = -i;
 	}
 	/* first remove the last digit of number and print  remaining digits */
 	/*using recursion, then print the last digit*/
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	if (i / 10)
+		print_number(i / 10);
+	_putchar(i % 10 + '0');
 }
