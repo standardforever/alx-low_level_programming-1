579cd6d58b6e212d10bdc9fc2e62fc6818adb515
standardforever <standard.forever123@gmail.com>
2022-04-24 10:39:11 -0700

    updated

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,21 +2,17 @@
 #define MAIN_H
 
 char *_strcat(char *dest, char *src);
-int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *c);
-char *cap_string(char *c);
-char *leet(char *c);
-char *rot13(char *c);
-<<<<<<< HEAD
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-=======
-void print_number(int n)
- char *infinite_add​(​char​ *n1, ​char​ *n2, ​char​ *r, ​int​ size_r)
->>>>>>> 2b331a3b7fb56254ec8577975bcfe94c61d8df86
+void print_buffer(char *b, int size);
+int _putchar(char);
 
 #endif
