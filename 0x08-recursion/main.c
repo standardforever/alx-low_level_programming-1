3fc417092a2c8bb1a2f44e8a13a49a9f39a939e7
standardforever <standard.forever123@gmail.com>
2022-04-28 15:21:09 -0700

    updated

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -8,6 +8,15 @@
  */
 int main(void)
 {
-	_print_rev_recursion("\nColton Walker");
+	int r;
+
+	r = factorial(1);
+	printf("%d\n", r);
+	r = factorial(5);
+	printf("%d\n", r);
+	r = factorial(10);
+	printf("%d\n", r);
+	r = factorial(-1024);
+	printf("%d\n", r);
 	return (0);
 }
