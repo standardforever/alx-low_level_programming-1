9de595744ef517f4cd4703d100d0a20946dff868
standardforever <standard.forever123@gmail.com>
2022-04-28 22:33:44 +0100

    update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse
- * @s: the string to be printed
- * Return: nothing
+ * _print_rev_recursion - main - check the code
+ * @s: character of a string
+ * Return: Always 0.
  */
-
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
@@ -13,6 +12,4 @@ void _print_rev_recursion(char *s)
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-	else
-		_putchar('\n');
 }
